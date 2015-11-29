71b70e167f4ae716a8995b71a22ccc851e3dac74
shouko <h999342@gmail.com>
2015-11-29 18:54:29 +0800

    fix malformed output for sticker

--- index.php
+++ index.php
@@ -13,17 +13,9 @@ $message = json_decode($_POST["message"], 1);
 if(!empty($message["attachments"]) && $message["attachments"][0]["type"] == "sticker"){
 	exit(json_encode(array("message" => array("sticker" => $message["attachments"][0]["stickerID"]), "threadID" => $message["threadID"])));
 }
-$stickers = array(
-		"jump" => "144884852352448"
-	);
-if($message["body"][0] == "*"){
-	$sticker_key = explode("*", $message["body"])[1];
-	if(isset($stickers[$sticker_key])){
-		exit(json_encode(array("sticker" => $stickers[$sticker_key], "threadID" => $message["threadID"])));
-	}
-}
 $responses = array(
 		"who are you" => "我是 Pusheen!"
+		"*jump*" => array("sticker" => "144884852352448")
 	);
 if(isset($responses[$message["body"]])){
 	exit(json_encode(array("message" => $responses[$message["body"]], "threadID" => $message["threadID"])));
