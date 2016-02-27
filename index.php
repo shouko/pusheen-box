8341eac892adafbe4363138f5b1b7fb1eced5ff0
shouko <h999342@gmail.com>
2016-02-27 12:14:54 +0800

    basic structure for command

--- index.php
+++ index.php
@@ -16,16 +16,69 @@ if(!empty($message["attachments"]) && $message["attachments"][0]["type"] == "sti
 	);
 	exit(json_encode($result));
 }
-$responses = array(
+$global_responses = array(
 		"who are you" => "我是 Pusheen!",
 		"*jump*" => array("sticker" => "144884852352448")
 	);
-if(isset($responses[$message["body"]])){
-	$result = array(
-		"message" => $responses[$message["body"]],
-		"threadID" => $message["threadID"]
-	);
-	exit(json_encode($result));
+$db = getDatabaseConnection();
+$command = explode(" ", $message["body"]);
+$data = array(
+  ':in_id' => $message["threadID"],
+  ':in_type' => $message["threadID"] == $message["senderID"] ? 1 : 0
+);
+isset($command[1]){
+  $data[':pattern'] = $command[1];
 }
-exit(json_encode(array("message" => utf8_strrev($message["body"]), "threadID" => $message["threadID"])));
+$response = array();
+switch($command[0]){
+  case "/add":
+    if(count($command) < 3){
+      break;
+    }
+    $sql = "INSERT INTO `pusheen_pattern` (`in_id`, `in_type`, `pattern`, `out_type`, `out_body`) VALUES (:in_id, :in_type, :pattern, 'body', :out_body)";
+    $stmt = $db->prepare($sql);
+    $data[':out_body'] = $command[2];
+    $stmt->execute($data);
+    $response["threadID"] = $message["threadID"];
+    $response["message"] = array(
+      "body" => "我知道惹！你說 $command[1] 我說 $command[2]";
+    )
+    break;
+  case "/del":
+    if(count($command) < 2){
+      break;
+    }
+    $sql = "DELETE FROM `pusheen_pattern` WHERE `in_id` = :in_id AND `in_type` = :in_type AND `pattern` = :pattern)";
+    $stmt = $db->prepare($sql);
+    $stmt->execute($data);
+    $response["threadID"] = $message["threadID"];
+    $response["message"] = array(
+      "body" => "我知道惹！";
+    )
+    break;
+  case "/query":
+    $sql = "SELECT `pattern`, `out_body` FROM `pusheen_pattern` WHERE `id_id` = :id_in AND `in_type` = :in_type";
+    $stmt = $db->prepare($sql);
+    $stmt->execute($data);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if(empty($result)){
+      $response["threadID"] = $message["threadID"];
+      $response["message"] = array(
+        "body" => "我知道惹！";
+      );
+    }else{
+        // list all
+    }
+    break;
+  default:
+    if(isset($global_responses[$message["body"]])){
+      $response["message"] = array(
+        "body" => $global_responses[$message["body"]]
+      ),
+      $response["threadID"] = $message["threadID"];
+    }
+    $sql = "SELECT "
+    die("");
+}
+exit(json_encode($response));
 ?>
