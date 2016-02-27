fe286f3adffed25192b9e3aad2a04ebc8112206f
shouko <h999342@gmail.com>
2016-02-27 11:36:42 +0800

    add init script

--- init.php
+++ init.php
@@ -1,2 +1,14 @@
 <?php
 include 'lib.php';
+
+$db = getDatabaseConnection();
+$db->query('CREATE TABLE IF NOT EXISTS `pusheen_pattern` (
+  `in_id` bigint(20) NOT NULL,
+  `in_type` tinyint(4) NOT NULL,
+  `pattern` varchar(30) NOT NULL,
+  `out_type` varchar(30) NOT NULL,
+  `out_body` varchar(255) NOT NULL,
+  CONSTRAINT pk_pattern PRIMARY KEY (`in_id`,`in_type`,`pattern`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+
+echo 'ok';
